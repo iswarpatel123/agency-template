1a18532f100a560986d791b397972829127818a9
Iswar Patel <ispatel@fb.com>
2025-01-07 12:22:54 -0800

    minor updates

--- src/utils/data/banner.ts
+++ src/utils/data/banner.ts
@@ -1,4 +1,4 @@
-const banner = ["🎉 HAPPY HOLIDAYS SALE!🎄 UP TO 60% OFF", "🚚FREE SHIPPING ON ALL ORDERS!", "🔒 100% ENCRYPTED & SECURE CHECKOUT"]
+const banner = ["🎉 NEW YEAR SALE!🎄 UP TO 60% OFF", "🚚 FREE SHIPPING ON ALL ORDERS!", "🔒 100% ENCRYPTED & SECURE CHECKOUT"]
 
 export {
     banner
